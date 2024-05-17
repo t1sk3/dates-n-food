d08a36370a80d25a422ea4899655f68441100740
t1sk3 <mattis.martens@gmail.com>
2024-05-18 00:38:32 +0200

    feat: Add error handling for invalid title in TV series form

--- src/app/pages/tv/add-tv-dialog/add-tv-dialog.component.ts
+++ src/app/pages/tv/add-tv-dialog/add-tv-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SupabaseService } from '../../../services/supabase.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-add-tv-dialog',
@@ -11,6 +12,7 @@ import { SupabaseService } from '../../../services/supabase.service';
 export class AddTvDialogComponent implements OnInit {
   dateForm: FormGroup;
   isMoviePage: boolean = true;
+  titleNotFound: boolean = false;
 
   constructor(
     private fb: FormBuilder, 
@@ -26,9 +28,24 @@ export class AddTvDialogComponent implements OnInit {
     this.isMoviePage = window.location.pathname.includes('movies');
   }
 
-  onSubmit(): void {
+  async onSubmit() {
     if (!this.dateForm.invalid) {
-      this.isMoviePage ? this.supabaseService.createMovie(this.dateForm.value) : this.supabaseService.createSeries(this.dateForm.value);
+      let data = await fetch('https://www.omdbapi.com/?apikey=' + environment.OMDB_API_KEY + '&t=' + this.dateForm.value.title.replace(' ', '+'))
+      .then(response => response.json())
+      .then(data => {
+        return data;
+      });
+      if (data.Error || data.Response === 'False') {
+        this.titleNotFound = true;
+        return;
+      }
+      let newData = {
+        title: data.Title,
+        imdb_id: data.imdbID,
+        seen: false,
+        liked: false,
+      };
+      this.isMoviePage ? this.supabaseService.createMovie(newData) : this.supabaseService.createSeries(newData);
     }
     // reload data
     this.dialogRef.close();
