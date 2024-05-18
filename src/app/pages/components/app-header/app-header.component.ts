2864d9d5de8160f11306b149483193a32f2ca1f5
t1sk3 <mattis.martens@gmail.com>
2024-05-18 20:54:02 +0200

    feat: Add mobile-friendly navigation drawer to app header component

--- src/app/pages/components/app-header/app-header.component.ts
+++ src/app/pages/components/app-header/app-header.component.ts
@@ -15,10 +15,22 @@ export class AppHeaderComponent {
   ) { }
 
   ngOnInit() {
+    // if screenwidth is less than 768px
+    if (window.innerWidth < 768) {
+      this.toggleSidenav()
+    }
   }
 
   toggleProfile() {
     console.log('toggling profile')
+  }
+
+  toggleSidenav() {
+    let list = document.getElementById('options-list')
+    if (list) {
+      list.style.height === '0px' ? list.style.height = 'auto' : list.style.height = '0px';
+      list.style.opacity === '0' ? list.style.opacity = '1' : list.style.opacity = '0';
+    }
   }
 
   logout() {
