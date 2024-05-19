494fcc642a62c29e3f3dacae42fec22ac1822887
t1sk3 <mattis.martens@gmail.com>
2024-05-19 22:29:09 +0200

    refactor: Update auth.guard.ts to use async/await for getUser() method

--- src/app/services/guards/auth.guard.ts
+++ src/app/services/guards/auth.guard.ts
@@ -2,8 +2,8 @@ import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { SupabaseService } from '../supabase.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = async (route, state) => {
   const supabaseService = inject(SupabaseService);
-  const user = supabaseService.getUser();
-  return user !== null;
+  const user = await supabaseService.getUser();
+  return user.user !== null;
 };
