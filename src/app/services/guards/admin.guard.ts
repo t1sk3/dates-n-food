c054bd119dc832ccbaf77a2e8845211afff3bf67
t1sk3 <mattis.martens@gmail.com>
2024-05-19 14:35:14 +0200

    refactor: Simplify admin guard logic in admin.guard.ts

--- src/app/services/guards/admin.guard.ts
+++ src/app/services/guards/admin.guard.ts
@@ -2,9 +2,5 @@ import { CanActivateFn } from '@angular/router';
 
 export const adminGuard: CanActivateFn = (route, state) => {
   const data = localStorage.getItem('userInfo');
-  console.log('data: ', data);
-  if (data !== null && JSON.parse(data)[0].role === 'admin') {
-    return true;
-  }
-  return false;
+  return data !== null && JSON.parse(data)[0].role === 'admin'
 };
