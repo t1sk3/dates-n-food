05e9094e97dcfb7669620588c25c1d433918da2f
t1sk3 <mattis.martens@gmail.com>
2024-05-20 20:14:20 +0200

    feat: add animation to seen and like buttons on tv page

--- src/app/pages/tv/tv-info/tv-info.component.ts
+++ src/app/pages/tv/tv-info/tv-info.component.ts
@@ -1,24 +1,49 @@
 import { Component, Input, ElementRef, HostListener } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { SupabaseService } from '../../../services/supabase.service';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-import { timeout } from 'rxjs';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-tv-info',
   templateUrl: './tv-info.component.html',
   styleUrl: './tv-info.component.sass',
   animations: [
-    trigger('changeState', [
+    trigger('changeSeen', [
       state('expand_circle_down', style({
         transform: 'scale(1.0)'
       })),
       state('radio_button_unchecked', style({
         transform: 'scale(0.5)'
       })),
-      transition('*=>expand_circle_down', animate('200ms')),
-      transition('*=>radio_button_unchecked', animate('200ms'))
-    ])
+      transition('*=>expand_circle_down', animate('200ms', keyframes([
+        style({ transform: 'scale(1.0)', offset: 0.5 }),
+        style({ transform: 'scale(1.2)', offset: 0.7 }),
+        style({ transform: 'scale(1.0)', offset: 1.0 })
+      ]))),
+      transition('*=>radio_button_unchecked', animate('200ms', keyframes([
+        style({ transform: 'scale(0.5)', offset: 0.5 }),
+        style({ transform: 'scale(0.3)', offset: 0.7 }),
+        style({ transform: 'scale(0.5)', offset: 1.0 })
+      ])))
+    ]),
+    trigger('changeLiked', [
+      state('expand_heart', style({
+        transform: 'scale(1.0)',
+      })),
+      state('heart_unchecked', style({
+        transform: 'scale(0.7)',
+      })),
+      transition('*=>expand_heart', animate('200ms', keyframes([
+        style({ transform: 'scale(1.0)', offset: 0.5 }),
+        style({ transform: 'scale(1.2)', offset: 0.7 }),
+        style({ transform: 'scale(1.0)', offset: 1.0 })
+      ]))),
+      transition('*=>heart_unchecked', animate('200ms', keyframes([
+        style({ transform: 'scale(0.5)', offset: 0.5 }),
+        style({ transform: 'scale(0.3)', offset: 0.7 }),
+        style({ transform: 'scale(0.5)', offset: 1.0 })
+      ])))
+    ]),
   ]
 })
 export class TvInfoComponent {
